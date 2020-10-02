301c536891163d8465ee7ed42aff28af6491cc76
Josef Friedrich <josef.friedrich@fau.de>
2020-10-02 13:24:09 +0200

    Die Fragen bearbeiten

--- FRAGEN/erzeuge-index-xml.sh
+++ FRAGEN/erzeuge-index-xml.sh
@@ -1,10 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 
 > index_neu.xml
 
-for PFAD in $(find . -iname "*.xml" | sort); do
-	echo $PFAD
+for PFAD in $(find . -type f \( -iname "*.xml" ! -iname "*index_neu.xml" ! -iname "*index.xml" \) | sort); do
+	PFAD="${PFAD:2}"
+  # echo "$PFAD"
 	THEMA="$(xmllint --xpath '//thema/text()' "$PFAD")"
-
 	echo "<themenGebiet pfad=\"$PFAD\" titel=\"$THEMA\"/>" >> index_neu.xml
 done
